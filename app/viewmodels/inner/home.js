cc01cb10d873d9e5e5dbba829a8be966f74e9137
Estevão Rolim <estevao.prolim@gmail.com>
2021-01-18 13:55:27 +0000

    Home page

--- app/viewmodels/inner/home.js
+++ app/viewmodels/inner/home.js
@@ -1,10 +1,41 @@
 ﻿define(['durandal/app'], function (app) {
     var page = function () {
 
+        var self = this;
 
-        this.activate = function () {
+        var iconRoute = './images/icons/';
+
+        self.sideNav = {
+            logo: {
+                active: './images/home/logo-horizontal-active.svg',
+                muted: './images/home/logo-horizontal-muted.svg',
+            },
+            notification: {
+                icon: './images/icons/notification.svg',
+                alt: 'Notificações'
+            },
+            collapsed: ko.observable(true),
+            id: '#navbarDropdown',
+            options: [
+                { route: '/', title: 'Configurações', icon: iconRoute + 'config.svg' },
+                { route: '/', title: 'Sobre', icon: iconRoute + 'info.svg' },
+                { route: '/', title: 'Minha Conta', icon: iconRoute + 'account.svg' },
+                { route: '/', title: 'Pagamento', icon: iconRoute + 'card.svg' },
+                { route: '/', title: 'Favoritos', icon: iconRoute + 'star.svg' },
+                { route: '#', title: 'Sair', icon: iconRoute + 'sign-out.svg' }
+            ]
+        };
+
+        self.clickedIt = () => {
+            var visible = !$(self.sideNav.id).hasClass("show");
+
+            $("#togglerImg").attr("src", (visible) ? self.sideNav.logo.active : self.sideNav.logo.muted );
+        };
+
+        self.activate = function () {
 
         }
+
         
     };
 
