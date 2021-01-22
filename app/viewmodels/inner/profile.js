b6f09380d8d29c1f18bacf56960a413603682a9f
Estevão Rolim <estevao.prolim@gmail.com>
2021-01-22 18:24:07 +0000

    profile

--- app/viewmodels/inner/profile.js
+++ app/viewmodels/inner/profile.js
@@ -1,15 +1,34 @@
 ﻿define(['durandal/app'], function (app) {
     var page = function () {
 
-        this.pages = [
-            { route: '/home', title: 'Home', icon: './images/icons/home.svg' },
-            { route: '/orders', title: 'Pedidos', icon: './images/icons/orders.svg' },
-            { route: '/search', title: 'Pesquisa', icon: './images/icons/search.svg' },
-            { route: '/profile', title: 'Perfil', icon: './images/icons/profile.svg' }
-        ];
+        self.title = 'Meu Perfil';
+
+        var imagesRoute = './images/profile/';
+
+        var nPosts = 5;
+        var filePosts = [imagesRoute + 'post', '.jpg']
+
+        self.posts = [];
+
+        [...Array(nPosts).keys()].forEach(key => {
+            var n = key + 1
+            self.posts.push({ img: filePosts[0] + n + filePosts[1] });
+        });
+
+        self.profile = {
+            username: 'alberto_ferr',
+            name: 'Alberto Ferreira',
+            desc: 'Amante de produção familiar e agricultura orgânica',
+            pic: imagesRoute + 'pic.jpg',
+            followers: 153,
+            following: 30,
+            posts: self.posts,
+        };
+
+        self.leftBtn = { text: 'Seguir' }
+        self.rightBtn = { text: 'Mensagem', icon: 'arrow-right-dark' } 
 
         this.activate = function (page) {
-
         }
         
     };
